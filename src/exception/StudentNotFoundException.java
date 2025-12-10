df781895411f06054ab01cf1af29a6221ff50205
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2025-12-10 15:41:34 +0000

    added a custom StudentNotFoundException class for handling errors

--- src/exception/StudentNotFoundException.java
+++ src/exception/StudentNotFoundException.java
@@ -1,7 +1,14 @@
 package exception;
 
 public class StudentNotFoundException extends Exception {
+
+    // Constructor with message only
     public StudentNotFoundException(String message) {
         super(message);
+    }
+
+    // Constructor with message and cause
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
